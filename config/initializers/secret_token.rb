701e280bfec1e91a9f2bd1262f2c1fe2c8c097b4
Louise Rains <lrains@berklee.edu>
2013-08-05 13:34:42 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stash41::Application.config.secret_key_base = '8710aaca7f65749a365f6944d981e24699654a71bfd13a1901ef6485ab91984e085979740ea167a59cfef30cf9be2646112e393f86ed750adac0efae7864e8dd'

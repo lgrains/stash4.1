c0f1e39f416e5197948d916b8f9b6a49da520fe9
Louise Rains <lrains@berklee.edu>
2013-08-09 15:44:23 -0400

    adds migration for listing

--- db/migrate/20130809193447_create_listings.rb
+++ db/migrate/20130809193447_create_listings.rb
@@ -1,6 +1,18 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
+      t.integer   :user_id, :null => false
+      t.string    :status
+      t.string    :image_url
+      t.string    :description
+      t.float     :length
+      t.float     :width
+      t.boolean   :is_treated
+      t.string    :treatment_method
+      t.string    :manufacturer
+      t.string    :fabric_line_name
+      t.string    :fabric_designer
+      t.float     :price
 
       t.timestamps
     end
